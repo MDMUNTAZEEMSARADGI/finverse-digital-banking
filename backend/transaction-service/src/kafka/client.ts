0a4370bd5ede1076692192fa73490d621ef2f646
Muntazeem <muntazeem.87@gmail.com>
2026-08-11 20:01:31 +0530

    configured kafka

--- backend/transaction-service/src/kafka/client.ts
+++ backend/transaction-service/src/kafka/client.ts
@@ -1,11 +1,40 @@
+// import { Kafka } from "kafkajs";
+// import fs from "fs";
+// import path from "path";
+
+// const caPath = path.join(process.cwd(), "certs", "ca.pem");
+
+// export const kafka = new Kafka({
+//   clientId: "finverse",
+
+//   brokers: [process.env.KAFKA_BROKER!],
+
+//   ssl: {
+//     ca: [fs.readFileSync(caPath, "utf-8")],
+//   },
+
+//   sasl: {
+//     mechanism: "scram-sha-256",
+//     username: process.env.KAFKA_USERNAME!,
+//     password: process.env.KAFKA_PASSWORD!,
+//   },
+// });
+
 import { Kafka } from "kafkajs";
+
+const ca = process.env.KAFKA_CA_CERT?.replace(/\\n/g, "\n");
 
 export const kafka = new Kafka({
   clientId: "finverse",
+
   brokers: [process.env.KAFKA_BROKER!],
-  ssl: true,
+
+  ssl: {
+    ca: [ca!],
+  },
+
   sasl: {
-    mechanism: "plain",
+    mechanism: "scram-sha-256",
     username: process.env.KAFKA_USERNAME!,
     password: process.env.KAFKA_PASSWORD!,
   },
