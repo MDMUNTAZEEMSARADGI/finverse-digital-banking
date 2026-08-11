8247a07068539f4702905afe7803eba7e7c478ea
Muntazeem <muntazeem.87@gmail.com>
2026-08-11 20:31:53 +0530

    prepared notification service

--- backend/notification-service/src/kafka/client.ts
+++ backend/notification-service/src/kafka/client.ts
@@ -1,10 +1,42 @@
+// FOR Render
 import { Kafka } from "kafkajs";
 
-export const kafka =
-  new Kafka({
-    clientId:
-      "notification-service",
-    brokers: [
-      process.env.KAFKA_BROKER!,
-    ],
-  });  
+const ca = process.env.KAFKA_CA_CERT?.replace(/\\n/g, "\n");
+
+export const kafka = new Kafka({
+  clientId: "notification-service",
+
+  brokers: [process.env.KAFKA_BROKER!],
+
+  ssl: {
+    ca: [ca!],
+  },
+
+  sasl: {
+    mechanism: "scram-sha-256",
+    username: process.env.KAFKA_USERNAME!,
+    password: process.env.KAFKA_PASSWORD!,
+  },
+});
+
+// import { Kafka } from "kafkajs";
+// import fs from "fs";
+// import path from "path";
+
+// const caPath = path.join(process.cwd(), "certs", "ca.pem");
+
+// export const kafka = new Kafka({
+//   clientId: "notification-service",
+
+//   brokers: [process.env.KAFKA_BROKER!],
+
+//   ssl: {
+//     ca: [fs.readFileSync(caPath, "utf-8")],
+//   },
+
+//   sasl: {
+//     mechanism: "scram-sha-256",
+//     username: process.env.KAFKA_USERNAME!,
+//     password: process.env.KAFKA_PASSWORD!,
+//   },
+// });
--- backend/notification-service/src/kafka/consumer.ts
+++ backend/notification-service/src/kafka/consumer.ts
@@ -2,51 +2,39 @@ import { kafka } from "./client";
 
 import { createNotification } from "../services/notification.service";
 
-import type {
-  NotificationEvent
-} from "../types/notification.types";
-
+import type { NotificationEvent } from "../types/notification.types";
 
 export const startConsumer = async () => {
-
   const consumer = kafka.consumer({
-    groupId:"notification-group",
+    groupId: "notification-group",
   });
-
 
   await consumer.connect();
 
-
   await consumer.subscribe({
-    topic:"transaction.created",
-    fromBeginning:true,
+    topic: "transaction.created",
+    fromBeginning: true,
   });
 
-
-  console.log(
-    "Notification Consumer Connected"
-  );
-
+  console.log("Notification Consumer Connected");
 
   await consumer.run({
+    eachMessage: async ({ message }) => {
+      try {
+        if (!message.value) return;
 
-  eachMessage: async ({ message }) => {
-  try {
-    if (!message.value) return;
+        const event = JSON.parse(
+          message.value.toString()
+        ) as NotificationEvent;
 
-    const event = JSON.parse(
-      message.value.toString()
-    ) as NotificationEvent;
+        console.log("Received Event:", event);
 
-    console.log("Received Event:", event);
+        await createNotification(event);
 
-    await createNotification(event);
-
-    console.log("Notification Saved");
-  } catch (error) {
-    console.error(error);
-  }
-}
+        console.log("Notification Saved");
+      } catch (error) {
+        console.error(error);
+      }
+    },
   });
-
 };
